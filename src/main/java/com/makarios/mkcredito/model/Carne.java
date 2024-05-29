d62f63c7d80a5b0749344b7bd89ccce44b407d88
Claudio <claudiocp.ads@gmail.com>
2024-05-29 10:49:19 -0300

    ajuste biblioteca

--- src/main/java/com/makarios/mkcredito/model/Carne.java
+++ src/main/java/com/makarios/mkcredito/model/Carne.java
@@ -1,4 +1,62 @@
 package com.makarios.mkcredito.model;
 
+import java.util.Date;
+
 public class Carne {
+    private int numero;
+    private String nomeCliente;
+    private Date dataVencimento;
+    private double valor;
+
+    // Construtor
+    public Carne(int numero, String nomeCliente, Date dataVencimento, double valor) {
+        this.numero = numero;
+        this.nomeCliente = nomeCliente;
+        this.dataVencimento = dataVencimento;
+        this.valor = valor;
+    }
+
+    // Métodos getters e setters
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public Date getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(Date dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    // Método toString para exibir informações sobre o carnê
+    @Override
+    public String toString() {
+        return "Carne{" +
+                "numero=" + numero +
+                ", nomeCliente='" + nomeCliente + '\'' +
+                ", dataVencimento=" + dataVencimento +
+                ", valor=" + valor +
+                '}';
+    }
 }
