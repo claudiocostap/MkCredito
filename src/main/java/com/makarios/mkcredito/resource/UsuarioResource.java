56a4c07260ab8d8bcc2ff0686b13be0f34b8032a
Claudio Costa <claudiocp.ads@gmail.com>
2025-10-19 13:18:02 -0300

    ajusta imports

--- src/main/java/com/makarios/mkcredito/resource/UsuarioResource.java
+++ src/main/java/com/makarios/mkcredito/resource/UsuarioResource.java
@@ -1,12 +1,18 @@
 package com.makarios.mkcredito.resource;
 
-import com.makarios.mkcredito.model.Usuario;
-import com.makarios.mkcredito.service.UsuarioService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.makarios.mkcredito.model.Usuario;
+import com.makarios.mkcredito.service.UsuarioService;
 
 import jakarta.validation.Valid;
 
